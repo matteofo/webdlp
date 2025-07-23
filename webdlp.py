9a9fcee9e558e49a56db37d627d2019c54d5b8fe
Matteo Forlani <matteo@matthew5pl.net>
2025-07-23 16:21:36 +0200

    first commit

---
+++ webdlp.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, Response
+from yt_dlp import YoutubeDL
+from contextlib import redirect_stdout
+from pathlib import Path
+import io, os
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    with open("./epic.html") as f:
+        return f.read()
+
+@app.route("/process")
+def audio():
+    video = request.args.get("id")
+    mode = request.args.get("mode")
+
+    ctx = {
+        "outtmpl": "-",
+        'logtostderr': True
+    }
+
+    if not mode:
+        ctx['extract_audio'] = True
+        ctx['format'] = 'bestaudio'
+
+    buffer = io.BytesIO()
+    with redirect_stdout(buffer), YoutubeDL(ctx) as yt:
+        yt.download(video)
+
+    dpath = f"{video}.mp4"
+    mime = "video/mp4"
+
+    if not mode:
+        dpath = f"{video}.mp3"
+        mime = "audio/mpeg"
+
+    Path(dpath).write_bytes(buffer.getvalue())
+    os.remove(dpath)
+    
+    return Response(buffer.getvalue(), mimetype=mime)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5106)
