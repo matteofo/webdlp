e2bac2aefe1ffb043a43f78c8b30cda77526b5c0
Matteo Forlani <matteo@matthew5pl.net>
2025-12-18 11:35:11 +0100

    add queue (should work?)

--- webdlp.py
+++ webdlp.py
@@ -2,11 +2,24 @@
 
 from flask import *
 from yt_dlp import YoutubeDL
-import os, git
+import os, git, uuid, threading
 from urllib.parse import urlparse, unquote, parse_qs
 
-app = Flask(__name__)
+class DownloadJob():
+    def __init__(self, thread: threading.Thread, vid: str, path: str, mime: str):
+        self.thread = thread
+        self.vid = vid
+        self.path = path
+        self.mime = mime
+        self.id = uuid.uuid4()
 
+app = Flask(__name__)
+jobs: list[DownloadJob] = []
+
+def download_thread(yt, video, dl_path):
+    print(f"WORKER: {yt} {video} {dl_path}")
+    yt.download(video)
+    
 def get_commit() -> str:
     repo = git.Repo("./")
     return repo.head.object.hexsha[:7]
@@ -23,6 +36,37 @@ def self_update():
 def root():
     self_update()
     return render_template("index.html", commit=get_commit(), commit_msg=get_commit_msg())
+    
+@app.route("/status")
+def status():
+    id = request.args.get("id")
+    if not id:
+        return Response("Invalid arguments", status=400)
+    
+    job = None
+    
+    for j in jobs:
+        if j.id.hex == id:
+            job = j
+            
+    if job is None:
+        return Response("No such job!", 404)
+            
+    if job.thread.is_alive():
+        resp = make_response(render_template("status.html", video_id=j.vid))
+        resp.headers.set("Refresh", "1")
+        return resp
+    else:
+        # read file to ram
+        f = open(j.path, 'rb')
+        contents = f.read()
+        f.close()
+        
+        os.remove(j.path)
+        
+        return Response(contents, mimetype=j.mime, content_type=j.mime, headers={
+            "Content-Disposition": "attachment; filename=" + j.path
+        })
 
 @app.route("/process")
 def process():
@@ -72,23 +116,16 @@ def process():
     # set download path
     ctx['outtmpl'] = dl_path
 
-    with YoutubeDL(ctx) as yt:
-        yt.download(video)
-    
-        # read file to ram
-        f = open(dl_path, 'rb')
-        contents = f.read()
-        f.close()
+    with YoutubeDL(ctx) as yt:      
+        dl_thread = threading.Thread(target=download_thread, args=[yt, video, dl_path])
+        job = DownloadJob(dl_thread, video, dl_path, mime)
+        jobs.append(job)
         
-        # delete from fs
-        os.remove(dl_path)
+        dl_thread.start()
         
-        # tell browser this is an attachment
-        headers = {
-            "Content-Disposition": "attachment; filename=" + dl_path
-        }
-
-        return Response(contents, mimetype=mime, content_type=mime, headers=headers)
+        return Response(status=308, headers={
+            "Location": "./status?id=" + job.id.hex
+        })
 
 if __name__ == "__main__":
     app.run(debug=True, port=5106, host="0.0.0.0")
